5fe157864e2b8f815955a6bc7b77beb818dd4329
Fvazquez <foapgenerico@365.stucom.com>
2025-05-16 17:39:05 +0200

    stucom

---
+++ controlador/conexion.php
@@ -0,0 +1,18 @@
+<?php
+
+// Datos de acceso a la Base de Datos
+// $host = "127.0.0.1";
+$host = "localhost";
+$database = "gestor_tareas";
+$port = 3307;
+$user = "usuario";
+$password = "usuario";
+
+
+try {
+    $conn = new PDO ("mysql:host=$host;port=$port;dbname=$database;", $user, $password );
+    
+
+} catch (PDOException $e) {
+    echo $e->getMessage();
+}
